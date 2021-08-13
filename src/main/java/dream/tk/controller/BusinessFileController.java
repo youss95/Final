0659c3d451c0961ce28396f9e46cff20de966c7d
yoonseo <riicoseo@naver.com>
2021-08-13 20:55:12 +0900

    edit2

---
+++ src/main/java/dream/tk/controller/BusinessFileController.java
@@ -0,0 +1,64 @@
+package dream.tk.controller;
+
+import java.io.File;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import dream.tk.service.BusinessFileService;
+
+@RequestMapping("/bFile")
+@Controller
+public class BusinessFileController {
+
+	@Autowired
+	private BusinessFileService fser;
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("upload")
+	public String upload(String id, MultipartFile[] file) {
+		String realPath = session.getServletContext().getRealPath("files");
+		
+		try {
+			fser.upload(realPath, id, file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "redirect:/bMember/myPage";
+	}
+	
+	
+	@RequestMapping("editFile")
+	public String editFile(String id, String[] delFiles, MultipartFile[] file) {
+		
+		String realPath = session.getServletContext().getRealPath("files");
+		try {
+			fser.upload(realPath, id, file);
+	
+		for(int i=0; i<delFiles.length; i++) {
+			String seq = delFiles[i];
+			String delfileName = fser.getSysName(seq);
+			File delTarget = new File(realPath+"/"+delfileName);
+			boolean delResult = delTarget.delete();
+			if(delResult) {
+			fser.delete(seq);
+			}
+		}
+		
+		
+		}catch (Exception e){		
+			e.printStackTrace();
+		}
+		
+		
+		return "redirect:/bMember/myPage";
+	}
+	
+	
+}
