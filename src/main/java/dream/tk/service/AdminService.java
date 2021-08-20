7a806a52aa09429dd1b21ab3a75a6c7021c44690
hiki <ye22im@gmail.com>
2021-08-20 09:28:55 +0900

    admin added

---
+++ src/main/java/dream/tk/service/AdminService.java
@@ -0,0 +1,20 @@
+package dream.tk.service;
+
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dream.tk.dao.AdminMemberDAO;
+
+@Service
+public class AdminService {
+
+	@Autowired
+	private AdminMemberDAO dao;
+
+	
+
+
+
+}
