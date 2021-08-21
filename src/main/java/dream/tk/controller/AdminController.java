baff4aa5c914c46ba9bd36c1b448cf91481a879f
yoonseo <riicoseo@naver.com>
2021-08-21 14:41:03 +0900

    blackList & error page

--- src/main/java/dream/tk/controller/AdminController.java
+++ src/main/java/dream/tk/controller/AdminController.java
@@ -4,8 +4,11 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import dream.tk.service.AdminService;
 import dream.tk.service.BusinessMemberService;
 import dream.tk.service.ClientMemberService;
 
@@ -14,47 +17,70 @@ import dream.tk.service.ClientMemberService;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
-	@Autowired
-	private HttpSession session;
+   
+   @Autowired
+   private HttpSession session;
 
-	@Autowired
-	private ClientMemberService serviceC;
-	
-	@Autowired
-	private BusinessMemberService serviceB;
-	
-	@RequestMapping(value="dashForm")
-	public String dashForm() {
-		return "admin/adminDashboard";
-	}
-	
-	@RequestMapping(value="insertForm")
-	public String insertForm() {
-		System.out.println("insertForm 요청");
-		return "admin/insert";
-	}
-	
-	@RequestMapping(value="insertC")
-	public String insertC() {
-		System.out.println("inserC 요청");
-		serviceC.insert();
-		return "admin/insert";
-	}
-//	@RequestMapping(value="signupProc")
-//	public String signupProc(AdminMemberDTO dto, Model m) {
-//		String shaPW = SHA256.getSHA512(dto.getPw());
-//		dto.setPw(shaPW);
-//		int result = service.insert(dto);
+   @Autowired
+   private ClientMemberService serviceC;
+   
+   @Autowired
+   private BusinessMemberService serviceB;
+   
+   @Autowired
+   private AdminService serviceA;
+   
+   
+   @RequestMapping(value="dashForm")
+   public String dashForm() {
+      return "admin/adminDashboard";
+   }
+   
+   @RequestMapping(value="insertForm")
+   public String insertForm() {
+      System.out.println("insertForm 요청");
+      return "admin/insert";
+   }
+   
+   @RequestMapping(value="insertC")
+   public String insertC() {
+      System.out.println("inserC 요청");
+      serviceC.insert();
+      return "admin/insert";
+   }
+   
+   @RequestMapping(value="insertB")
+   public String insertB() {
+      System.out.println("inserB 요청");
+      //serviceB.insert();
+      return "admin/insert";
+   }
+//   @RequestMapping(value="signupProc")
+//   public String signupProc(AdminMemberDTO dto, Model m) {
+//      String shaPW = SHA256.getSHA512(dto.getPw());
+//      dto.setPw(shaPW);
+//      int result = service.insert(dto);
 //
-//		m.addAttribute("result", result);
-//		return "memberA/signupResult";
-//	}
-	
-//	@ResponseBody
-//	@RequestMapping(value="emailConfirm")
-//	public String emailConfirm(String name, String email) throws Exception {
-//		return serviceE.sendEmailConfirm(name, email);
-//	}
-	
+//      m.addAttribute("result", result);
+//      return "memberA/signupResult";
+//   }
+   
+//   @ResponseBody
+//   @RequestMapping(value="emailConfirm")
+//   public String emailConfirm(String name, String email) throws Exception {
+//      return serviceE.sendEmailConfirm(name, email);
+//   }
+   
+   	@RequestMapping("manageBusinessPage")
+   	public String manageBusiness(Model m) {
+   	 System.out.println(serviceA.getBlacklist());
+   	  m.addAttribute("blackList",serviceA.getBlacklist());
+      return "admin/manageBusiness";
+   	}
+   	
+   	@ResponseBody
+	@RequestMapping("businessOut")
+   	public int businessOut(int seq) {
+   	return serviceA.businessOut(seq);
+   	}
 }
--- src/main/java/dream/tk/service/AdminService.java
+++ src/main/java/dream/tk/service/AdminService.java
@@ -1,10 +1,12 @@
 package dream.tk.service;
 
-import java.sql.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import dream.tk.dao.AdminDAO;
 import dream.tk.dao.AdminMemberDAO;
 
 @Service
@@ -12,9 +14,16 @@ public class AdminService {
 
 	@Autowired
 	private AdminMemberDAO dao;
-
 	
+	@Autowired
+	private AdminDAO Adao;
 
+	public List<Map<String,String>>getBlacklist(){
+		return Adao.getBlackList();
+	}
 
+	public int businessOut(int seq){
+		return Adao.businessOut(seq);
+	}
 
 }
