ad2bde960af8895d215d4b2437ceb04ebd318e2d
johnsina <lgz0712@naver.com>
2021-08-23 21:05:19 +0900

    *

--- src/main/java/dream/tk/controller/BusinessStoreController.java
+++ src/main/java/dream/tk/controller/BusinessStoreController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import dream.tk.config.PagingVO;
 import dream.tk.dto.BusinessDTO;
 import dream.tk.dto.LikeStatusDTO;
+import dream.tk.dto.TranslateDTO;
 import dream.tk.service.LikesService;
 import dream.tk.service.StoreBusinessService;
 import dream.tk.service.TranslateService;
@@ -73,17 +74,22 @@ public class BusinessStoreController {
 	}
 	
 	//디테일 페이지로
-	@RequestMapping("view")
-	public String view(int biz_seq,String userId, Model m, String store) throws Exception{
-		BusinessDTO dto = service.select(biz_seq);
-		LikeStatusDTO statusDto = new LikeStatusDTO(biz_seq,userId);
-		String likeStatus = likeService.likeStatus(statusDto);
-		//m.addAttribute("countCmt", serviceC.count(store_seq));
-		m.addAttribute("list", dto);
-		m.addAttribute("likeStatus",likeStatus);
-		session.setAttribute("store", store);
-		return "/Store/BusinessStoreDetail";
-	}
+	   @RequestMapping("view")
+	   public String view(int biz_seq,String userId, Model m, String store) throws Exception{
+	      BusinessDTO dto = service.select(biz_seq);
+	      LikeStatusDTO statusDto = new LikeStatusDTO(biz_seq,userId);
+	      String likeStatus = likeService.likeStatus(statusDto);
+
+	      List<TranslateDTO> menuList = serviceT.select(biz_seq);
+	      m.addAttribute("menuList", menuList);
+
+	      //m.addAttribute("countCmt", serviceC.count(store_seq));
+	      m.addAttribute("list", dto);
+	      m.addAttribute("likeStatus",likeStatus);
+	      session.setAttribute("store", store);
+	      return "/Store/BusinessStoreDetail";
+	   }
+	
 	
 	// 혹시 모르는 페이지
 	@RequestMapping("store_detail")
